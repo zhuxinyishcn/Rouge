0f19e73e79b1e613cb0ffb2c80dd2b75812d7588
Xinyi <zhuxinyishcn@outlook.com>
2020-04-29 18:10:18 -0500

    initial commit

---
+++ src/test/java/rouge/code/community/RougeApplicationTests.java
@@ -0,0 +1,13 @@
+package rouge.code.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
